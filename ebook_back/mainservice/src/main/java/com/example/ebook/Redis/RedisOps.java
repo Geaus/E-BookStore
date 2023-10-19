5e2f1c72c5cd0017701632e3dce1a21b10aa28e1
gxj2003 <2061389208@qq.com>
2023-10-19 22:55:19 +0800

    ll

---
+++ ebook_back/mainservice/src/main/java/com/example/ebook/Redis/RedisOps.java
@@ -0,0 +1,45 @@
+package com.example.ebook.Redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisOps {
+    @Autowired
+    RedisTemplate redisTemplate;
+
+    public Object get(String key){
+        try{
+            return redisTemplate.opsForValue().get(key);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Redis 连接出现异常");
+            return null;
+        }
+    }
+
+    public void set(String key,Object value){
+        try{
+            redisTemplate.opsForValue().set(key,value);
+
+        }catch (Exception e){
+//            e.printStackTrace();
+            System.out.println("Redis 连接出现异常");
+            return;
+        }
+    }
+
+    public void delete(String key){
+        try{
+            redisTemplate.delete(key);
+
+        }catch (Exception e){
+//            e.printStackTrace();
+            System.out.println("Redis 连接出现异常");
+            return;
+        }
+    }
+
+}
--- ebook_back/src/main/java/com/example/ebook/Redis/RedisOps.java
+++
@@ -1,2 +0,0 @@
-package com.example.ebook.Redis;public class redisOps {
-}
